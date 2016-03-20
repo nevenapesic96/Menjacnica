ace193c0cb82518b464570dcc29c35996c0e0dd4
nevenapesic96 <npnevenap@gmail.com>
2016-03-20 23:08:17 +0100

    Napravljene klase Valuta i Kurs, generisane get i set metode

---
+++ menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,35 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+	private GregorianCalendar datum;
+	private double prodajni;
+	private double kupovni;
+	private double srednji;
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	
+}
