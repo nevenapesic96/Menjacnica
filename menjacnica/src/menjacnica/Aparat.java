5781031869086009f77e808ef313106bc534fd56
nevenapesic96 <npnevenap@gmail.com>
2016-03-23 00:59:19 +0100

    Dodata klasa Aparat koja implementira menjacnicaInterfejs

---
+++ menjacnica/src/menjacnica/Aparat.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.menjacnicaInterfejs;
+
+public class Aparat implements menjacnicaInterfejs{
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, Kurs kurs, Valuta valuta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKurs(GregorianCalendar datum, Kurs kurs, Valuta valuta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(GregorianCalendar datum, Valuta valuta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
