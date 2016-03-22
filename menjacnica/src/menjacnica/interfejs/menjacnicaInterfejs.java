5c8d22a2f8e8834f9654768b00841a0d5b98a89d
nevenapesic96 <npnevenap@gmail.com>
2016-03-23 00:55:34 +0100

    Dodat parametar valuta u menjacnicaInterfejs.

--- menjacnica/src/menjacnica/interfejs/menjacnicaInterfejs.java
+++ menjacnica/src/menjacnica/interfejs/menjacnicaInterfejs.java
@@ -3,9 +3,10 @@ package menjacnica.interfejs;
 import java.util.GregorianCalendar;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 
 public interface menjacnicaInterfejs {
-	public void dodajKurs(GregorianCalendar datum, Kurs kurs);
-	public void izbrisiKurs(GregorianCalendar datum, Kurs kurs);
-	public Kurs vratiKurs(GregorianCalendar datum);
+	public void dodajKurs(GregorianCalendar datum, Kurs kurs, Valuta valuta);
+	public void izbrisiKurs(GregorianCalendar datum, Kurs kurs, Valuta valuta);
+	public Kurs vratiKurs(GregorianCalendar datum, Valuta valuta);
 }
